6ca3a8c6f3115c9a42692760ef3a93fcff7e5108
Daniel Griffiths <logiconictvtube@gmail.com>
2018-02-17 16:32:18 -0500

    added descriptions to skills data

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -3,161 +3,145 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
 
-    // TODO add other parameters to have cool interavtivity with the user, so they can learn about the skills
-    // TODO Allow visitors to add skills if they are not present
     const skills = [
       {
         id: 1,
         skillName: 'Rails',
         skillLevel: 8,
-        description: 'This is a description',
+        description: "Ruby on Rails, or Rails, is a server-side web application framework written in Ruby under the MIT License. Rails is a model–view–controller (MVC) framework, providing default structures for a database, a web service, and web pages.",
         stack: 'Back End'
       },
       {
         id: 2,
         skillName: 'Ruby',
         skillLevel: 9,
-        description: 'This is a description',
+        description: "Ruby is a dynamic, reflective, object-oriented, general-purpose programming language. It was designed and developed in the mid-1990s by Yukihiro Matsumoto in Japan. According to its creator, Ruby was influenced by Perl, Smalltalk, Eiffel, Ada, and Lisp.",
         stack: 'Back End'
       },
       {
         id: 3,
         skillName: 'Javascript',
         skillLevel: 9,
-        description: 'This is a description',
+        description: "JavaScript® (often shortened to JS) is a lightweight, interpreted, object-oriented language with first-class functions, and is best known as the scripting language for Web pages, but it's used in many non-browser environments as well. It is a prototype-based, multi-paradigm scripting language that is dynamic, and supports object-oriented, imperative, and functional programming styles.",
         stack: 'Front End'
       },
       {
         id: 4,
         skillName: 'Java',
         skillLevel: 4,
-        description: 'This is a description',
+        description: "Java is a general-purpose computer-programming language that is concurrent, class-based, object-oriented, and specifically designed to have as few implementation dependencies as possible.",
         stack: 'Back End'
       },
       {
         id: 5,
         skillName: 'Python',
         skillLevel: 4,
-        description: 'This is a description',
+        description: "Python is an interpreted, object-oriented, high-level programming language with dynamic semantics. ... Python's simple, easy to learn syntax emphasizes readability and therefore reduces the cost of program maintenance. Python supports modules and packages, which encourages program modularity and code reuse.",
         stack: 'Back End'
       },
       {
         id: 6,
         skillName: 'PHP',
         skillLevel: 7,
-        description: 'This is a description',
+        description: "PHP (recursive acronym for PHP: Hypertext Preprocessor) is a widely-used open source general-purpose scripting language that is especially suited for web development and can be embedded into HTML.",
         stack: 'Back End'
       },
       {
         id: 7,
         skillName: 'Laravel',
         skillLevel: 6,
-        description: 'This is a description',
+        description: "Laravel is a free, open-source PHP web framework, created by Taylor Otwell and intended for the development of web applications following the model–view–controller (MVC) architectural pattern.",
         stack: 'Back End'
       },
       {
         id: 8,
         skillName: 'HTML',
         skillLevel: 10,
-        description: 'This is a description',
+        description: "The <meta> tag provides metadata about the HTML document. ... Meta elements are typically used to specify page description, keywords, author of the document, last modified, and other metadata. The metadata can be used by browsers (how to display content or reload page), search engines (keywords), or other web services.",
         stack: 'Front End'
       },
       {
         id: 9,
         skillName: 'CSS',
         skillLevel: 10,
-        description: 'This is a description',
+        description: "Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language. CSS is designed primarily to enable the separation of presentation and content, including aspects such as the layout, colors, and fonts.",
         stack: 'Front End'
       },
       {
         id: 10,
         skillName: 'SQL',
         skillLevel: 9,
-        description: 'This is a description',
+        description: "Structured Query Language (SQL) is a standard computer language for relational database management and data manipulation. SQL is used to query, insert, update and modify data.",
         stack: 'Back End'
       },
       {
         id: 11,
-        skillName: 'MySQL',
+        skillName: 'Git',
         skillLevel: 9,
-        description: 'This is a description',
-        stack: 'Back End'
+        description: "Git is a version control system for tracking changes in computer files and coordinating work on those files among multiple people. It is primarily used for source code management in software development, but it can be used to keep track of changes in any set of files.",
+        stack: 'Auxillary'
       },
       {
         id: 12,
-        skillName: 'Git',
-        skillLevel: 9,
-        description: 'This is a description',
+        skillName: 'cPanel',
+        skillLevel: 3,
+        description: "cPanel is an online Linux-based web hosting control panel that provides a graphical interface and automation tools designed to simplify the process of hosting a web site.",
         stack: 'Auxillary'
       },
       {
         id: 13,
-        skillName: 'PostgreSQL',
-        skillLevel: 9,
-        description: 'This is a description',
-        stack: 'Back End'
+        skillName: 'jQuery',
+        skillLevel: 8,
+        description: "jQuery is a concise and fast JavaScript library that can be used to simplify event handling, HTML document traversing, Ajax interactions and animation for speedy website development. jQuery simplifies the HTML's client-side scripting, thus simplifying Web 2.0 applications development.",
+        stack: 'Front End'
       },
       {
         id: 14,
-        skillName: 'cPanel',
-        skillLevel: 3,
-        description: 'This is a description',
-        stack: 'Auxillary'
+        skillName: 'Ajax',
+        skillLevel: 7,
+        description: "Ajax is a client-side script that communicates to and from a server/database without the need for a postback or a complete page refresh.",
+        stack: 'Front End'
       },
       {
         id: 15,
-        skillName: 'jQuery',
-        skillLevel: 8,
-        description: 'This is a description',
+        skillName: 'AngularJS',
+        skillLevel: 9,
+        description: "AngularJS (commonly referred to as 'Angular.js' or 'AngularJS 1.X') is a JavaScript-based open-source front-end web application framework mainly maintained by Google and by a community of individuals and corporations to address many of the challenges encountered in developing single-page applications.",
         stack: 'Front End'
       },
       {
         id: 16,
-        skillName: 'Ajax',
-        skillLevel: 7,
-        description: 'This is a description',
+        skillName: 'ReactJS',
+        skillLevel: 2,
+        description: "React allows developers to create large web-applications that use data and can change over time without reloading the page. It aims primarily to provide speed, simplicity, and scalability. React processes only user interfaces in applications. This corresponds to View in the Model-View-Controller (MVC) pattern, and can be used in combination with other JavaScript libraries or frameworks in MVC, such as AngularJS.",
         stack: 'Front End'
       },
       {
         id: 17,
-        skillName: 'AngularJS',
-        skillLevel: 9,
-        description: 'This is a description',
-        stack: 'Front End'
+        skillName: 'AWS',
+        skillLevel: 4,
+        description: "Amazon Web Services (AWS) is a secure cloud services platform, offering compute power, database storage, content delivery and other functionality to help businesses scale and grow.",
+        stack: 'Auxillary'
       },
       {
         id: 18,
-        skillName: 'ReactJS',
-        skillLevel: 2,
-        description: 'This is a description',
+        skillName: 'SCSS',
+        skillLevel: 9,
+        description: "Sass (Syntactically awesome style sheets) is a style sheet language initially designed by Hampton Catlin and developed by Natalie Weizenbaum. After its initial versions, Weizenbaum and Chris Eppstein have continued to extend Sass with SassScript, a simple scripting language used in Sass files.",
         stack: 'Front End'
       },
       {
         id: 19,
-        skillName: 'AWS',
+        skillName: 'CloudFlare',
         skillLevel: 4,
-        description: 'This is a description',
+        description: "Cloudflare, Inc. is a U.S. company that provides a content delivery network, DDoS mitigation, Internet security services and distributed domain name server services, sitting between the visitor and the Cloudflare user's hosting provider, acting as a reverse proxy for websites.",
         stack: 'Auxillary'
       },
       {
         id: 20,
-        skillName: 'SCSS',
-        skillLevel: 9,
-        description: 'This is a description',
-        stack: 'Front End'
-      },
-      {
-        id: 21,
-        skillName: 'CloudFlare',
-        skillLevel: 4,
-        description: 'This is a description',
-        stack: 'Auxillary'
-      },
-      {
-        id: 22,
         skillName: 'Wordpress',
         skillLevel: 6,
-        description: 'This is a description',
+        description: "Meta description is a brief description of what your page or post is about. Meta keywords and description are actually HTML meta tags, and they go into your website's header. WordPress SEO plugins make it super easy for you to add them from your WordPress admin area for each post and page on your website.",
         stack: 'Auxillary'
       }
     ];
@@ -217,9 +201,5 @@ export class InMemoryDataService implements InMemoryDbService {
       }
     ];
     return {skills, repos};
-
-
-
-    // TODO Add Biography, Repository information, Contact information, etc...
   }
 }
