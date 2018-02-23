2eb691f893f8c897f6d1424ab3098bc24d3838b6
Daniel Griffiths <logiconictvtube@gmail.com>
2018-02-22 19:24:13 -0500

    Added contact page and structured basics

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as $ from 'jquery';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
 
@@ -11,5 +14,32 @@ export class ContactComponent implements OnInit {
 
   ngOnInit() {
   }
+  ngAfterViewInit() {
+    var $cell = $('.card');
 
+    $cell.find('.js-expander').click(function() {
+      var $thisCell = $(this).closest('.card');
+
+      if ($thisCell.hasClass('is-collapsed')) {
+        $cell.not($thisCell).removeClass('is-expanded').addClass('is-collapsed').addClass('is-inactive');
+        $thisCell.removeClass('is-collapsed').addClass('is-expanded');
+
+        if ($cell.not($thisCell).hasClass('is-inactive')) {
+        } else {
+          $cell.not($thisCell).addClass('is-inactive');
+        }
+      }
+      else {
+        $thisCell.removeClass('is-expanded').addClass('is-collapsed');
+        $cell.not($thisCell).removeClass('is-inactive');
+      }
+    });
+
+    $cell.find('.js-collapser').click(function() {
+      var $thisCell = $(this).closest('.card');
+
+      $thisCell.removeClass('is-expanded').addClass('is-collapsed');
+      $cell.not($thisCell).removeClass('is-inactive');
+    });
+  }
 }
--- src/app/skill-search/skill-search.component.ts
+++ src/app/skill-search/skill-search.component.ts
@@ -24,24 +24,15 @@ export class SkillSearchComponent implements OnInit {
 
   constructor(private skillService: SkillService) {}
 
-  // Push a search term into the observable stream.
   search(term: string): void {
     this.searchTerms.next(term);
   }
 
   ngOnInit(): void {
     this.skills$ = this.searchTerms.pipe(
-      // wait 300ms after each keystroke before considering the term
       debounceTime(300),
-
-      // ignore new term if same as previous term
       distinctUntilChanged(),
-
-      // switch to new search observable each time the term changes
       switchMap((term: string) => this.skillService.searchSkills(term)),
     );
-  }
-
-  ngAfterViewInit() {
   }
 }
